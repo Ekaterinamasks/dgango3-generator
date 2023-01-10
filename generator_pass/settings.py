7e493094f51109f3c2616b989a49f3cf33c5159a
Kate <chegodaeva-96@mail.ru>
2023-01-10 10:30:20 +0300

    I made soem tweaks

--- generator_pass/settings.py
+++ generator_pass/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-=$tv&97evg1en!+8h7ck2w^5#wkh)ln)mbf)%lvyj=h2vuv8h&"
+SECRET_KEY = "django-insecure-=$tv&97evg1en!+8h7ckghjg2w^5#wkh)ln)mbf)%lvyj=h2vuv8h&"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
